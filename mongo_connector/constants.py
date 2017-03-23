536775046f93c05c8df083e79c85a10fb49d6d25
peter721 <peter721@users.noreply.github.com>
2017-03-23 16:05:45 -0500

    Update constants.py

--- mongo_connector/constants.py
+++ mongo_connector/constants.py
@@ -22,7 +22,7 @@ DEFAULT_COMMIT_INTERVAL = None
 
 # Maximum # of documents to send in a single bulk request through a
 # DocManager.
-DEFAULT_MAX_BULK = 10000
+DEFAULT_MAX_BULK = 1000
 
 # The default MongoDB field that will serve as the unique key for the
 # target system.
